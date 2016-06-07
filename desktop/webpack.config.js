e1978cfe825b6ff290f527e3b528b9f75514034e
Lance Stout <lancestout@gmail.com>
2016-06-07 09:34:21 -0700

    Fix react-native+babel issue by removing babelrc file

--- desktop/webpack.config.js
+++ desktop/webpack.config.js
@@ -44,7 +44,10 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader'
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
             }
         ]
     }
