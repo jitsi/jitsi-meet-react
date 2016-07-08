23c58a6f25d8bbca52ab81caeeadb9b962404728
Lance Stout <lancestout@gmail.com>
2016-07-08 08:09:41 -0700

    Use single quotes to match our style

--- webpack.config.js
+++ webpack.config.js
@@ -45,11 +45,11 @@ module.exports = {
             // in our test regex.
             {
                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&minetype=application/font-woff"
+                loader: 'url-loader?limit=10000&minetype=application/font-woff'
             },
             {
                 test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader"
+                loader: 'file-loader'
             },
             // Disable AMD for Strophe and its plugins because we don't know how
             // to require them successfully.
