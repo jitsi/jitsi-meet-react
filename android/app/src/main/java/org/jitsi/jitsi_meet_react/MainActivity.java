005dec31494a89146f3f16b6bd3dbd0a912a2447
paweldomas <pawel.domas@jitsi.org>
2016-07-27 15:12:16 -0500

    Capture Jitsi Meet URLs on Android

--- android/app/src/main/java/org/jitsi/jitsi_meet_react/MainActivity.java
+++ android/app/src/main/java/org/jitsi/jitsi_meet_react/MainActivity.java
@@ -1,9 +1,57 @@
 package org.jitsi.jitsi_meet_react;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
+
+    /**
+     * The log tag used in MainActivity class.
+     */
+    private final static String TAG = MainActivity.class.getCanonicalName();
+
+    /**
+     * Holds the url passed as an argument of the ACTION_VIEW intent.
+     */
+    private String url = null;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            // On ACTION_VIEW intent there should be a link provided in
+            // the Intent's data. Passing whatever value has been specified in
+            // order to eventually react with an error on the JS side if it's
+            // invalid.
+            this.url = String.valueOf(intent.getData());
+            Log.d(TAG, "ACTION_VIEW URL: " + this.url);
+        }
+        // Execute 'super' after the processing, otherwise 'getLanuchOptions'
+        // will be called before the 'url' field is set
+        super.onCreate(savedInstanceState);
+    }
+
+    /**
+     * Overrides parent method in order to provide the URL in the 'initialProps'
+     * passed by React Native to the app.
+     *
+     * @return a <tt>Bundle</tt> which will contain 'url' property if this
+     * instance has been started by the ACTION_VIEW intent.
+     */
+    @Override
+    protected Bundle getLaunchOptions() {
+        Bundle bundle = super.getLaunchOptions();
+        if (bundle == null) {
+            bundle = new Bundle();
+        }
+        if (this.url != null) {
+            bundle.putCharSequence("url", this.url);
+        }
+        return bundle;
+    }
 
     /**
      * {@inheritDoc}
