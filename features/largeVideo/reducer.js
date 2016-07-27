532ad5dfc0641ce599df3f6989cb201229149780
tsareg <tsaregradskyi@gmail.com>
2016-07-27 12:10:10 +0300

    Fix local participant was not shown on large video after merge

--- features/largeVideo/reducer.js
+++ features/largeVideo/reducer.js
@@ -1,3 +1,8 @@
+import {
+    CONFERENCE_JOINED,
+    CONFERENCE_LEFT
+} from '../base/conference';
+import { LOCAL_PARTICIPANT_DEFAULT_ID } from '../base/participants';
 import { ReducerRegistry } from '../base/redux';
 
 import { LARGE_VIDEO_PARTICIPANT_CHANGED } from './actionTypes';
@@ -9,6 +14,34 @@ const INITIAL_STATE = {
 ReducerRegistry.register('features/largeVideo',
     (state = INITIAL_STATE, action) => {
         switch (action.type) {
+        /**
+         * When conference is joined, we update ID of local participant from
+         * default 'local' to real ID. However in large video we might have
+         * already selected 'local' as participant on stage. So in this case
+         * we must update ID of participant on stage to match ID in
+         * 'participants' state to avoid additional changes in state and
+         * rerenders.
+         */
+        case CONFERENCE_JOINED:
+            if (state.participantId === LOCAL_PARTICIPANT_DEFAULT_ID) {
+                let id = action.conference.myUserId();
+
+                return {
+                    ...state,
+                    participantId: id
+                };
+            }
+
+            return state;
+        /**
+         * Reverse operation to CONFERENCE_JOINED.
+         */
+        case CONFERENCE_LEFT:
+            return {
+                ...state,
+                participantId: LOCAL_PARTICIPANT_DEFAULT_ID
+            };
+
         case LARGE_VIDEO_PARTICIPANT_CHANGED:
             return {
                 ...state,
