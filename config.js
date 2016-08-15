e26cc1242dc2bb7a5a4177705b05385391027e45
tsareg <tsaregradskyi@gmail.com>
2016-08-15 11:50:50 +0300

    disableThirdPartyRequests in config.js

--- config.js
+++ config.js
@@ -43,4 +43,13 @@ if (!configObject) { // Default connection configuration
     };
 }
 
+// FIXME Lib-jitsi-meet uses HTML script elements to asynchronously
+// load certain pieces of JavaScript. Unfortunately, the technique
+// doesn't work on React Native (because there are no HTML elements
+// in the first place). Fortunately, these pieces of JavaScript
+// currently involve third parties and we can temporarily disable
+// them (until we implement an alternative to async script elements
+// on React Native).
+configObject.disableThirdPartyRequests = true;
+
 export default configObject;
